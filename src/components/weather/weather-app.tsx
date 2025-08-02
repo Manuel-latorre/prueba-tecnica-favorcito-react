e252d058f036d66aadfd4647d93aff938dc16db8
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-02 15:02:45 -0300

    feat: implement WeatherApp component to manage weather data display and loading states

---
+++ src/components/weather/weather-app.tsx
@@ -0,0 +1,50 @@
+import { useWeatherStore } from '@/store/weatherStore';
+import {
+  CitySearch,
+  ErrorMessage,
+  CurrentWeatherSkeleton,
+  WeatherForecastSkeleton,
+  CitySearchSkeleton
+} from '@/components/index';
+import WeatherDataDisplay from './weather-data';
+import { useWeatherInitialization } from '@/hooks/useWeather';
+
+
+export function WeatherApp() {
+  const { hasData, loading, error } = useWeatherInitialization();
+
+  const {
+    location,
+    currentWeather,
+    forecast,
+  } = useWeatherStore();
+
+  return (
+    <div className="min-h-dvh flex flex-col items-center justify-center max-md:p-4 py-10">
+      <div className="max-w-4xl mx-auto space-y-6 w-full">
+        <div className="flex justify-center">
+          {loading ? <CitySearchSkeleton /> : <CitySearch />}
+        </div>
+
+        {error && (
+          <div className="max-w-md mx-auto flex justify-center mb-4">
+            <ErrorMessage message={error} />
+          </div>
+        )}
+
+        {loading ? (
+          <div className='flex flex-col gap-4 w-full justify-center items-center'>
+            <CurrentWeatherSkeleton />
+            <WeatherForecastSkeleton />
+          </div>
+        ) : hasData && (
+          <WeatherDataDisplay
+            currentWeather={currentWeather!}
+            forecast={forecast!}
+            cityName={location!.name}
+          />
+        )}
+      </div>
+    </div>
+  );
+} 
