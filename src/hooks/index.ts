e308640324771439ce3a433c560fbffec7b6f8a1
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-02 00:26:17 -0300

    feat: create index file to export all weather hooks for streamlined access

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+// Export all weather hooks
+export { useGeocoding } from './useGeocoding';
+export { useCurrentWeather } from './useCurrentWeather';
+export { useForecast } from './useForecast';
+export { useWeather } from './useWeather'; 
