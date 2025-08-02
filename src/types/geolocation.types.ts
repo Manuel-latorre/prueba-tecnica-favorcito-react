6d0655d6a636d8274e2731cb71c7f9db64001972
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-02 11:03:51 -0300

    feat: add UseGeocodingReturn interface for geocoding functionality and location management

--- src/types/geolocation.types.ts
+++ src/types/geolocation.types.ts
@@ -1,4 +1,14 @@
+import type { WeatherLocation } from "./weather.types";
+
 export interface GeolocationPosition {
     latitude: number;
     longitude: number;
+}
+
+export interface UseGeocodingReturn {
+    location: WeatherLocation | null;
+    loading: boolean;
+    error: string | null;
+    searchLocation: (city: string) => Promise<void>;
+    reset: () => void;
   }
