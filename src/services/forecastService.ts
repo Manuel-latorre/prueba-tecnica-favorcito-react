dcec9e7ad97705bc70e4a46f09d2281ff5cdb5cb
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-02 00:20:56 -0300

    feat: implement forecast service to fetch weather forecast data based on latitude and longitude

---
+++ src/services/forecastService.ts
@@ -0,0 +1,25 @@
+import {
+  type OpenMeteoForecastResponse,
+} from "@/types/weather.types";
+import { API_CONFIG } from "@/config/api.config";
+import { fetchWithTimeout } from "@/utils/fetchUtils";
+
+export async function getForecast(
+  lat: number,
+  lon: number
+): Promise<OpenMeteoForecastResponse> {
+  try {
+    const response = await fetchWithTimeout(
+      `${API_CONFIG.BASE_URL}/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=7`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error obteniendo pronóstico:", error);
+    throw error;
+  }
+} 
