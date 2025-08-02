13249c1c73ee7a7cff1a581c10bac80c6f6dc886
Manuel-latorre <manuel.latorre11@gmail.com>
2025-08-02 10:57:38 -0300

    refactor: update hooks exports to include useCitySearch for improved functionality

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,4 +2,5 @@
 export { useGeocoding } from './useGeocoding';
 export { useCurrentWeather } from './useCurrentWeather';
 export { useForecast } from './useForecast';
-export { useWeather } from './useWeather'; 
+export { useWeather } from './useWeather';
+export { useCitySearch } from './useCitySearch'; 
